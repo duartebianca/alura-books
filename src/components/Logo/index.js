3d73e5cf4d41be753306f940686397df1c9beb2f
duartebianca <biancaduarte1914@gmail.com>
2024-05-29 09:00:56 -0300

    feat: creation of the Logo component

---
+++ src/components/Logo/index.js
@@ -0,0 +1,12 @@
+import logo from '../../images/logo.svg'
+import './style.css'
+
+function Logo() {
+    return (
+        <div className = "logo">
+        <img src={logo} alt="logo"></img>
+        <p><strong>Alura</strong>Books</p>
+      </div>
+    )
+}
+export default Logo
