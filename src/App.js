788b964a38d68304163a8a9bdfa8b5f9fab17dee
duartebianca <biancaduarte1914@gmail.com>
2024-05-29 10:01:28 -0300

    refactor(App.js, App.css): refactoring options list

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
 import Logo from './components/Logo';
+
+const optionsText= ['CATEGORIAS', 'FAVORITOS', 'MINHA ESTANTE']
 function App() {
   return (
     <div className="App">
       <header className="App-header">
        <Logo></Logo>
+       <ul className = "options">
+          {optionsText.map((text) => (
+            <li className='option'><p>{text}</p></li>
+          ))}
+       </ul>
       </header>
     </div>
   );
