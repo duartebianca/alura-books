3dfe68582abcb832a72434673acf33e1eb5479d1
duartebianca <biancaduarte1914@gmail.com>
2024-05-29 11:07:48 -0300

    refactor: refactoring Header, Header Icons, and Header Options into components

--- src/App.js
+++ src/App.js
@@ -1,30 +1,9 @@
 import './App.css';
-import Logo from './components/Logo';
-import perfil from './images/perfil.svg'
-import sacola from './images/sacola.svg'
-
-const optionsText= ['CATEGORIAS', 'FAVORITOS', 'MINHA ESTANTE']
-const icons = [perfil, sacola]
-
+import Header from './components/Header';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-       <Logo></Logo>
-       <ul className = "options">
-          {optionsText.map((text) => (
-            <li className='option'><p>{text}</p></li>
-          ))}
-       </ul>
-        <ul className = "icons">
-            {icons.map((icon) => (
-              <li className='icon'><
-                img src={icon}
-                alt='icon'
-                ></img></li>
-            ))}
-        </ul>
-      </header>
+    <Header/>
     </div>
   );
 }
