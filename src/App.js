dfd577c9dd3562ffc705db953e7a41ae3d6c5618
duartebianca <biancaduarte1914@gmail.com>
2024-05-29 08:02:36 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import logo from './images/logo.svg';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className = "logo">
+          <img src={logo} alt="logo"></img>
+          <p><strong>Alura</strong>Books</p>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
